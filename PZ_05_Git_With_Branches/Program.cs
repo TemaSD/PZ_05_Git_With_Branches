e3affd924803869c78f4c89f386029a8b52a7230
Artem Shupa-Dubrova <fxtema@gmail.com>
2024-03-15 22:27:59 +0200

    PZ_4_1

---
+++ PZ_05_Git_With_Branches/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PZ_05_Git_With_Branches
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Завдання 1
+
+            int[] numbers = new int[50];
+
+            int a = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                a += 2;
+                numbers[i] = a;
+                Console.WriteLine($"Element: {a}");
+            }
+        }
+    }
+}
